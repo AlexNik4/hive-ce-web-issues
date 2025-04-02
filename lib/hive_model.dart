810ed66cac854bb31b7543e9a035c46150872848
AlexNik4 <gallantalex8@gmail.com>
2025-04-01 21:45:36 -0400

    Referencing issue and working implementatin with draft fix

--- lib/hive_model.dart
+++ lib/hive_model.dart
@@ -1,17 +1,14 @@
 import 'package:hive_ce/hive.dart';
-import 'package:json_annotation/json_annotation.dart';
 
-part 'hive_model.g.dart';
-
-@JsonSerializable()
 class HiveModel extends HiveObject {
   final int id;
   final String name;
 
   HiveModel({required this.id, required this.name});
 
-  factory HiveModel.fromJson(Map<String, dynamic> json) =>
-      _$HiveModelFromJson(json);
+  factory HiveModel.fromJson(Map<String, dynamic> json) {
+    return HiveModel(id: json['id'] as int, name: json['name'] as String);
+  }
 
-  Map<String, dynamic> toJson() => _$HiveModelToJson(this);
+  Map<String, dynamic> toJson() => {'id': id, 'name': name};
 }
